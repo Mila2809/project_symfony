e3a9ac269750a03309a482ff640b564a6fecaf79
Aldric <aldric.vendas@gmail.com>
2024-12-27 02:20:51 +0100

    feat: create commandes

--- src/Controller/CommandesController.php
+++ src/Controller/CommandesController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Commandes;
+use App\Entity\Utilisateur;
+use App\Form\CommandesType;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CommandesController extends AbstractController
 {
-    #[Route('/commandes', name: 'app_commandes')]
-    public function index(): Response
-    {
+    #[Route('/commandes', name: 'app_commandes', methods: ['GET', 'POST'])]
+    public function index(UtilisateurRepository $utilisateurRepository, EntityManagerInterface $em, Request $request): Response
+    {   
+        $user = $this->getUser();
+        
+        $commandes = new Commandes();
+        $commandes->setUtilisateur($user);
+        $form = $this->createForm(CommandesType::class, $commandes);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em->persist($commandes);
+            $em->flush();
+            return $this->redirectToRoute('app_commandes');
+        }
+
+        $commandes = $em->getRepository(Commandes::class)->findAll();
         return $this->render('commandes/index.html.twig', [
-            'controller_name' => 'CommandesController',
+            'commandes' => $commandes,
+            'form' => $form,
         ]);
     }
 }
