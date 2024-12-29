fd44d0e65d40b64ec47c4e5be8d282d9858a3fe9
Aldric <aldric.vendas@gmail.com>
2024-12-29 04:06:41 +0100

    feat: commentaire

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,96 +2,129 @@
 
 namespace App\Controller;
 
-use App\Entity\Commandes;
-use App\Entity\ContenuPanier;
-use App\Entity\Produit;
-use App\Form\CommandesType;
-use App\Form\ContenuPanierType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use App\Form\ProduitType;
-use App\Repository\ProduitRepository;
+use App\Entity\Commandes;
+use App\Form\CommandesType;
+use App\Entity\ContenuPanier;
+use App\Form\ContenuPanierType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use App\Entity\Produit;
+use App\Repository\ProduitRepository;
+use App\Form\ProduitType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
+// Controller lié aux pages des produits
 #[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/all', name: 'app_produit_all', methods: ['GET'])]
-    public function index(ProduitRepository $produitRepository): Response
-    {
-        return $this->render('produit/index.html.twig', [
-            'produits' => $produitRepository->findAll(),
-        ]);
-    }
+
+    #region Create
     
+    // Création d'un produit
     #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
     public function new(EntityManagerInterface $em, Request $request): Response
     {
+        // Création du formulaire
         $produit = new Produit();
         $form = $this->createForm(ProduitType::class, $produit);
-
         $form->handleRequest($request);
+
+        // Envoie du formulaire
         if($form->isSubmitted() && $form->isValid()){
+            // Récupration de l'image
             /** @var UploadedFile $imageFile */
             $imageFile = $form->get('Photo')->getData();
 
             if ($imageFile) {
+                // Renommage de l'image
                 $newFilename = uniqid().'.'.$imageFile->guessExtension();
 
                 try {
+                    // Enregistrement de l'image
                     $imageFile->move(
                         $this->getParameter('upload_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    // METTRE UN MESSAGE D'ERREUR
+                    die($e);
                 }
 
+                // Ajout de la nouvelle image dans le formulaire
                 $produit->setPhoto($newFilename);
             }
+            // Envoie du formulaire à la BDD
             $em->persist($produit);
             $em->flush();
+
+            // Redirection vers la page
             return $this->redirectToRoute('app_produit_all');
         }
 
+        // Redirection vers la page
         return $this->render('produit/new.html.twig', [
             'form' => $form,
         ]);
     }
 
+    #endregion
+
+
+
+    #region Read All
+
+    // Affichage de la liste des produits
+    #[Route('/all', name: 'app_produit_all', methods: ['GET'])]
+    public function index(ProduitRepository $produitRepository): Response
+    {
+        // Redirection vers la page
+        return $this->render('produit/index.html.twig', [
+            'produits' => $produitRepository->findAll(),
+        ]);    
+    }    
+
+    #endregion
+
+
+
+    #region Read One
+
+    // Affichage des détails d'un produit et ajout au panier
     #[Route('/show/{id}', name: 'app_produit_selected', methods: ['GET', 'POST'])]
     public function selected(EntityManagerInterface $em, Request $request, Produit $produit): Response
     {
-
+        // Récupération de l'utilisateur
         $user = $this->getUser();
-        
-        $commandes = new Commandes();
-        $panier = new ContenuPanier();
-
+        // Récupération du panier de l'utilisateur
         $userPanier = $em->getRepository(Commandes::class)->findOneBy([
             'Utilisateur' => $user,
             'DateAchat' => null,
         ]);
-
+        
+        // Création du formulaire de commande
+        $commandes = new Commandes();
         $commandes->setUtilisateur($user);
-        $panier->setProduit($produit);
-        $panier->setCommandes($userPanier);
-
         $commandesForm = $this->createForm(CommandesType::class, $commandes);
-        $panierForm = $this->createForm(ContenuPanierType::class, $panier);
-
         $commandesForm->handleRequest($request);
+
+        // Création du formulaire du contenu de la commande
+        $contenuPanier = new ContenuPanier();
+        $contenuPanier->setProduit($produit);
+        $contenuPanier->setCommandes($userPanier);
+        $contenuPanierForm = $this->createForm(ContenuPanierType::class, $contenuPanier);
+        $contenuPanierForm->handleRequest($request);
+
+        // Envoie du formulaire de commande à la BDD
         if($commandesForm->isSubmitted()){
             $em->persist($commandes);
             $em->flush();
             return $this->redirectToRoute('app_produit_all');
         }
-
-        $panierForm->handleRequest($request);
-        if($panierForm->isSubmitted()){
+        // Envoie du formulaire du contenu de la commande à la BDD
+        if($contenuPanierForm->isSubmitted()){
+            // Si l'utilisateur n'a pas de panier, création d'un panier
             if (!$userPanier) {
                 $userPanier = new Commandes();
                 $userPanier->setUtilisateur($user);
@@ -99,77 +132,114 @@ class ProduitController extends AbstractController
                 $em->flush();
             }
 
+            // Recherche d'un produit identique dans la panier
             $produitPanier = $em->getRepository(ContenuPanier::class)->findOneBy([
                 'commandes' => $userPanier,
                 'Produit' => $produit,
             ]);
-
+            // Si le produit est déjà dans le panier, modification de la quantité
             if ($produitPanier){
-                $newQuantity = $produitPanier->getQuantite() + $panier->getQuantite();
-                $panier->setQuantite($newQuantity);
+                // Calcul et modification de la quantité de produit
+                $newQuantite = $produitPanier->getQuantite() + $contenuPanier->getQuantite();
+                $contenuPanier->setQuantite($newQuantite);
+                // Suppression de l'ancien produit
                 $em->remove($produitPanier);
-                $em->persist($panier);
+                // Envoie d'un nouveau produit avec la nouvelle quantité dans à la BDD
+                $em->persist($contenuPanier);
                 $em->flush();
+            // Sinon, ajout du produit
             } else {
-                $panier->setProduit($produit);
-                $panier->setCommandes($userPanier);
-                $em->persist($panier);
+                // Définission du produit et du panier
+                $contenuPanier->setProduit($produit);
+                $contenuPanier->setCommandes($userPanier);
+                // Envoie du formulaire à la BDD
+                $em->persist($contenuPanier);
                 $em->flush();
             }
-                return $this->redirectToRoute('app_produit_all');
+            // Redirection vers la page
+            return $this->redirectToRoute('app_produit_all');
         }
 
-        $commandes = $em->getRepository(ContenuPanier::class)->findAll();
-        $panier = $em->getRepository(ContenuPanier::class)->findAll();
-
+        // Redirection vers la page
         return $this->render('produit/selected.html.twig', [
             'produit' => $produit,
-            'panierForm' => $panierForm,
+            'contenuPanierForm' => $contenuPanierForm,
         ]);
     }
 
-    #[Route('/delete/{id}', name: 'app_produit_delete', methods: ['POST'])]
-    public function delete(Request $request, EntityManagerInterface $em, Produit $produit): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $produit->getId(), $request->request->get('csrf'))) {
-            $em->remove($produit);
-            $em->flush();
-        }
-        return $this->redirectToRoute('app_produit_all');
-    }
+    #endregion
 
+
+
+    #region Update
+
+    // Modification d'un produit
     #[Route('/edit/{id}', name: 'app_produit_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
     {
+        // Création du formulaire
         $form = $this->createForm(ProduitType::class, $produit);
         $form->handleRequest($request);
 
-            
+        // Envoie du formulaire
         if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération de l'image
             /** @var UploadedFile $imageFile */
             $imageFile = $form->get('Photo')->getData();
 
+            // Suppression de l'ancienne image
             unlink(__DIR__ . '/../../public/uploads/' . $produit->getPhoto());
 
             if ($imageFile) {
+                // Renommage de l'image
                 $newFilename = uniqid().'.'.$imageFile->guessExtension();
 
                 try {
+                    // Enregistrement de l'image
                     $imageFile->move(
                         $this->getParameter('upload_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {}
                 
+                // Ajout de la nouvelle image
                 $produit->setPhoto($newFilename);
             }
 
+            // Envoie du formulaire à la BDD
             $entityManager->flush();
+
+            // Redirection vers la page
             return $this->redirectToRoute('app_produit_selected', ['id' => $produit->getId()]);
         }
-            return $this->render('produit/edit.html.twig', [
-                'form' => $form->createView(),
-                'produit' => $produit,
-            ]);
+
+        // Redirection vers la page
+        return $this->render('produit/edit.html.twig', [
+            'form' => $form->createView(),
+            'produit' => $produit,
+        ]);
     }
+
+    #endregion
+
+
+
+    #region Delete
+
+    // Chemin de suppression d'un produit
+    #[Route('/delete/{id}', name: 'app_produit_delete', methods: ['POST'])]
+    public function delete(Request $request, EntityManagerInterface $em, Produit $produit): Response
+    {
+        // Suppression du produit
+        if ($this->isCsrfTokenValid('delete' . $produit->getId(), $request->request->get('csrf'))) {
+            $em->remove($produit);
+            $em->flush();
+        }
+
+        // Redirection vers la page
+        return $this->redirectToRoute('app_produit_all');
+    }
+
+    #endregion
+
 }
--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,87 +2,137 @@
 
 namespace App\Controller;
 
-use App\Entity\Utilisateur;
-use App\Form\UtilisateurType;
-use App\Repository\UtilisateurRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
+use App\Form\UtilisateurType;
 
+// Controller lié aux pages utilisateur
 #[Route('/utilisateur')]
 final class UtilisateurController extends AbstractController
 {
-    #[Route('/all', name: 'app_utilisateur_index', methods: ['GET'])]
-    public function index(UtilisateurRepository $utilisateurRepository): Response
-    {
-        return $this->render('utilisateur/index.html.twig', [
-            'utilisateurs' => $utilisateurRepository->findAll(),
-        ]);
-    }
 
+    #region Create
+
+    // Création d'un utilisateur
     #[Route('/new', name: 'app_utilisateur_new', methods: ['GET', 'POST'])]
     public function new(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        // Création du formulaire
         $utilisateur = new Utilisateur();
         $form = $this->createForm(UtilisateurType::class, $utilisateur);
         $form->handleRequest($request);
 
+        // Envoie du formulaire
         if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération du mot de passe
             /** @var string $plainPassword */
             $plainPassword = $form->get('Password')->getData();
-
-            // encode the plain password
+            // Hachage du mot de passe
             $utilisateur->setPassword($userPasswordHasher->hashPassword($utilisateur, $plainPassword));
             
+            // Envoie du formulaire avec le MDP haché à la BDD
             $entityManager->persist($utilisateur);
             $entityManager->flush();
 
+            // Redirection vers la page
             return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Redirection vers la page
         return $this->render('utilisateur/new.html.twig', [
             'utilisateur' => $utilisateur,
             'form' => $form,
         ]);
     }
 
+    #endregion
+
+
+
+    #region Read All
+
+    // Affiche de tous les utilisateurs
+    #[Route('/all', name: 'app_utilisateur_index', methods: ['GET'])]
+    public function index(UtilisateurRepository $utilisateurRepository): Response
+    {
+        // Redirection vers la page
+        return $this->render('utilisateur/index.html.twig', [
+            'utilisateurs' => $utilisateurRepository->findAll(),
+        ]);
+    }
+
+    #endregion
+
+
+
+    #region Read One
+
+    // Affiche d'un utilisateur
     #[Route('/{id}', name: 'app_utilisateur_show', methods: ['GET'])]
     public function show(Utilisateur $utilisateur): Response
     {
+        // Redirection vers la page
         return $this->render('utilisateur/show.html.twig', [
             'utilisateur' => $utilisateur,
         ]);
     }
 
+    #endregion
+
+
+
+    #region Update
+
+    // Modification d'un utilisateur
     #[Route('/{id}/edit', name: 'app_utilisateur_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
     {
+        // Création du formulaire
         $form = $this->createForm(UtilisateurType::class, $utilisateur);
         $form->handleRequest($request);
 
+        // Envoie du formulaire
         if ($form->isSubmitted() && $form->isValid()) {
+            // Envoie du formulaire à la BDD
             $entityManager->flush();
 
+            // Redirection vers la page
             return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Redirection vers la page
         return $this->render('utilisateur/edit.html.twig', [
             'utilisateur' => $utilisateur,
             'form' => $form,
         ]);
     }
 
+    #endregion
+
+
+
+    #region Delete
+
+    // Chemin de suppression d'un utilisateur
     #[Route('/{id}', name: 'app_utilisateur_delete', methods: ['POST'])]
     public function delete(Request $request, Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
     {
+        // Suppression de l'utilisateur
         if ($this->isCsrfTokenValid('delete'.$utilisateur->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($utilisateur);
             $entityManager->flush();
         }
 
+        // Redirection vers la page
         return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #endregion
+
 }
